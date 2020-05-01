03bbebbac546b02870c7452a878db63fbf493d54
Miaohan Wang <miaohan@midway2-login1.rcc.local>
2020-05-01 10:58:23 -0500

    finish task 1

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def plot_csv(csv_filename):
     df = pd.read_csv(csv_filename, names=['cores', 'run_time'])
-
+    df.sort_values(by='cores', inplace=True)
     df.plot(x='cores', y='run_time')
     plt.ylabel('time (s)')
     plt.title('Program Running Time (s) From 1 to 20 Cores')
